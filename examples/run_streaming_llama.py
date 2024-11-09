aebb489e60edb4c15edac8881de26cdc243822b4
kangki <3210106167@zju.edu.cn>
2024-11-09 10:31:47 +0800

    update the local address of model

--- examples/run_streaming_llama.py
+++ examples/run_streaming_llama.py
@@ -6,6 +6,7 @@ import torch
 import argparse
 import json
 import os
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
 import time
 import re
 import sys
@@ -111,7 +112,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--model_name_or_path", type=str, default="lmsys/vicuna-13b-v1.3"
+        "--model_name_or_path", type=str, default="C:/Users/y1116/.cache/huggingface/hub/models--lmsys--vicuna-13b-v1.3/snapshots/6566e9cb1787585d1147dcf4f9bc48f29e1328d2"#"lmsys/vicuna-13b-v1.3"
     )
     parser.add_argument("--data_root", type=str, default="data/")
     parser.add_argument("--enable_streaming", action="store_true")
